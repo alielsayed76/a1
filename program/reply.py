90e370d4705f67a28c716f28cdadf98c400fc685
alielsayed76 <98778110+alielsayed76@users.noreply.github.com>
2022-05-30 21:33:27 +0200

    Update reply.py

--- program/reply.py
+++ program/reply.py
@@ -51,10 +51,10 @@ async def nftb(client: Client, message: Message):
     )
     
     
-@Client.on_message(command("ايدي"))
+@Client.on_message(filter_command("ايدي"))
 def ids(client: Client, message: Message):
-    elketib = message.reply_to_message
-    if elketib:
+    ali = message.reply_to_message
+    if ali:
         message.reply_text(
             f"Name: {message.from_user.mention()}\nid: {massage.from_user.id}\nUserName: @{massage.from_user.username}"
         )
