04aa676663d00bd8d4cd753cc1be7c00d7acb259
alielsayed76 <98778110+alielsayed76@users.noreply.github.com>
2022-05-31 10:06:52 +0200

    Update reply.py

--- program/reply.py
+++ program/reply.py
@@ -123,14 +123,12 @@ async def motawerf(client: Client, message: Message):
 @Client.on_message(command(["تبتي"]) & filters.user(5002164804))
 async def motawer(client: Client, message: Message):
     await message.reply(
-        f"""مبرمج السورس حبيب قلبي 🌚🙈""",
-    )
+        f"""مبرمج السورس حبيب قلبي 🌚🙈""")
 
 
 @Client.on_message(command(["تبتي"]) & filters.user(5369052737))
 async def motawer(client: Client, message: Message):
     await message.reply(
-        f"""مبرمج السورس حبيب قلبي 🌚🙈""",
-    )
+        f"""مبرمج السورس حبيب قلبي 🌚🙈""")
 
 
